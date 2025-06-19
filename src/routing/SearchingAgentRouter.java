abfa40c1787136ef648de81e37ba816b8749add3
nara <98963440+vianneynara@users.noreply.github.com>
2025-06-20 00:59:02 +0700

    refactor: register router for static reset

--- src/routing/SearchingAgentRouter.java
+++ src/routing/SearchingAgentRouter.java
@@ -17,30 +17,45 @@ import java.util.*;
  * @author Jordan, Nara
  */
 public class SearchingAgentRouter implements RoutingDecisionEngine, SearchingAgentReporting {
-    /**
-     * The movement model class name of the target nodes.
-     */
-    public static String TARGET_MOVEMENT_S = "targetMovement";
-    /**
-     * The address prefix of the target nodes.
-     */
-    public static String TARGET_PREFIX_S = "targetPrefix";
-    /**
-     * The movement model class name of the target nodes.
-     */
-    public static String TARGET_ROUTER_S = "targetRouter";
+	/**
+	 * The movement model class name of the target nodes.
+	 */
+	public static String TARGET_MOVEMENT_S = "targetMovement";
+	/**
+	 * The address prefix of the target nodes.
+	 */
+	public static String TARGET_PREFIX_S = "targetPrefix";
+	/**
+	 * The movement model class name of the target nodes.
+	 */
+	public static String TARGET_ROUTER_S = "targetRouter";
 
-    public static String searchingAgentPrefix = "A";
+	public static String searchingAgentPrefix = "A";
 
-    protected static Set<DTNHost> encounteredSearchables;
-    private static double initialDiscovery;
+	protected static Set<DTNHost> discoveredTargets;
+	private static double initialDiscovery;
 
-//    private MessageRouter targetRouter;
-    private static MovementModel targetMovement;
-    private static String targetPrefix;
+	//    private MessageRouter targetRouter;
+	private static MovementModel targetMovement;
+	private static String targetPrefix;
 
-    public SearchingAgentRouter(Settings s) {
-        s.setNameSpace("SearchingAgentRouter");
+	/* Registers the router for reset. */
+	static {
+		DTNSim.registerForReset(SearchingAgentRouter.class.getCanonicalName());
+		reset();
+	}
+
+	/**
+	 * Resets the {@link #discoveredTargets} and {@link #initialDiscovery} to its original state}.
+	 * This is called when the simulation is reset.
+	 */
+	public static void reset() {
+		discoveredTargets = new HashSet<>();
+		initialDiscovery = 0;
+	}
+
+	public SearchingAgentRouter(Settings s) {
+		s.setNameSpace("SearchingAgentRouter");
 //        if (s.contains(TARGET_ROUTER_S)) {
 //            try {
 //                targetRouter = Class.forName(TARGET_ROUTER_S);
@@ -50,43 +65,43 @@ public class SearchingAgentRouter implements RoutingDecisionEngine, SearchingAge
 //        } else {
 //            targetRouter = null;
 //        }
-        if (s.contains(TARGET_MOVEMENT_S)) {
-            targetMovement = (MovementModel) s.createIntializedObject("movement." + s.getSetting(TARGET_MOVEMENT_S));
-        } else {
-            targetMovement = null;
-        }
-        if (s.contains(TARGET_PREFIX_S)) {
-            targetPrefix = s.getSetting(TARGET_PREFIX_S);
-        } else {
-            targetPrefix = "S";
-        }
+		if (s.contains(TARGET_MOVEMENT_S)) {
+			targetMovement = (MovementModel) s.createIntializedObject("movement." + s.getSetting(TARGET_MOVEMENT_S));
+		} else {
+			targetMovement = null;
+		}
+		if (s.contains(TARGET_PREFIX_S)) {
+			targetPrefix = s.getSetting(TARGET_PREFIX_S);
+		} else {
+			targetPrefix = "S";
+		}
 
-        if (encounteredSearchables == null) {
-            encounteredSearchables = new HashSet<>();
-        }
-    }
+		if (discoveredTargets == null) {
+			discoveredTargets = new HashSet<>();
+		}
+	}
 
-    public SearchingAgentRouter(SearchingAgentRouter sar) {
-        super();
-    }
+	public SearchingAgentRouter(SearchingAgentRouter sar) {
+		super();
+	}
 
-    /**
-     * Determines whether the peer is the target nodes to be searched,
-     * then inserts it into {@link #encounteredSearchables}.
-     * */
-    @Override
-    public void connectionUp(DTNHost thisHost, DTNHost peer) {
-        /* Ignore same routers / of same group */
-        if (peer.getGroupId().startsWith(searchingAgentPrefix) || peer.getGroupId().equals(thisHost.getGroupId()) || peer.getRouter() == thisHost.getRouter()) {
-            return;
-        }
-        if (targetMovement != null && targetMovement.getClass().isInstance(peer.getMovement())) {
-            System.out.println("FOUND HOST MOV: " + peer.getName());
-            if (initialDiscovery == 0) {
-                initialDiscovery = SimClock.getTime();
-            }
-            encounteredSearchables.add(peer);
-        }
+	/**
+	 * Determines whether the peer is the target nodes to be searched,
+	 * then inserts it into {@link #discoveredTargets}.
+	 */
+	@Override
+	public void connectionUp(DTNHost thisHost, DTNHost peer) {
+		/* Ignore same routers / of same group */
+		if (peer.getGroupId().startsWith(searchingAgentPrefix) || peer.getGroupId().equals(thisHost.getGroupId()) || peer.getRouter() == thisHost.getRouter()) {
+			return;
+		}
+		if (targetMovement != null && targetMovement.getClass().isInstance(peer.getMovement())) {
+			System.out.println("FOUND HOST MOV: " + peer.getName());
+			if (initialDiscovery == 0) {
+				initialDiscovery = SimClock.getTime();
+			}
+			discoveredTargets.add(peer);
+		}
 //        else if (targetRouter != null && targetRouter.isInstance(peer.getRouter())) {
 //            System.out.println("FOUND HOST: " + peer.getAddress());
 //            if (initialDiscovery == 0) {
@@ -94,89 +109,89 @@ public class SearchingAgentRouter implements RoutingDecisionEngine, SearchingAge
 //            }
 //            encounteredSearchables.add(peer);
 //        }
-        else if (peer.getName().startsWith(targetPrefix)) {
-            System.out.println("FOUND HOST PRE: " + peer.getName());
-            if (initialDiscovery == 0) {
-                initialDiscovery = SimClock.getTime();
-            }
-            encounteredSearchables.add(peer);
-        }
-    }
+		else if (peer.getName().startsWith(targetPrefix)) {
+			System.out.println("FOUND HOST PRE: " + peer.getName());
+			if (initialDiscovery == 0) {
+				initialDiscovery = SimClock.getTime();
+			}
+			discoveredTargets.add(peer);
+		}
+	}
 
-    @Override
-    public void connectionDown(DTNHost thisHost, DTNHost peer) {
-    }
+	@Override
+	public void connectionDown(DTNHost thisHost, DTNHost peer) {
+	}
 
-    @Override
-    public void doExchangeForNewConnection(Connection con, DTNHost peer) {
-    }
+	@Override
+	public void doExchangeForNewConnection(Connection con, DTNHost peer) {
+	}
 
-    @Override
-    public boolean newMessage(Message m) {
-        return true;
-    }
+	@Override
+	public boolean newMessage(Message m) {
+		return true;
+	}
 
-    @Override
-    public boolean isFinalDest(Message m, DTNHost aHost) {
-        return m.getTo() == aHost;
-    }
+	@Override
+	public boolean isFinalDest(Message m, DTNHost aHost) {
+		return m.getTo() == aHost;
+	}
 
-    @Override
-    public boolean shouldSaveReceivedMessage(Message m, DTNHost thisHost) {
-        return !thisHost.getRouter().hasMessage(m.getId());
-    }
+	@Override
+	public boolean shouldSaveReceivedMessage(Message m, DTNHost thisHost) {
+		return !thisHost.getRouter().hasMessage(m.getId());
+	}
 
-    @Override
-    public boolean shouldSendMessageToHost(Message m, DTNHost otherHost, DTNHost thisHost) {
-        return true;
-    }
+	@Override
+	public boolean shouldSendMessageToHost(Message m, DTNHost otherHost, DTNHost thisHost) {
+		return true;
+	}
 
-    @Override
-    public boolean shouldDeleteSentMessage(Message m, DTNHost otherHost) {
-        return false;
-    }
+	@Override
+	public boolean shouldDeleteSentMessage(Message m, DTNHost otherHost) {
+		return false;
+	}
 
-    @Override
-    public boolean shouldDeleteOldMessage(Message m, DTNHost hostReportingOld) {
-        return false;
-    }
+	@Override
+	public boolean shouldDeleteOldMessage(Message m, DTNHost hostReportingOld) {
+		return false;
+	}
 
-    @Override
-    public RoutingDecisionEngine replicate() {
-        return new SearchingAgentRouter(this);
-    }
+	@Override
+	public RoutingDecisionEngine replicate() {
+		return new SearchingAgentRouter(this);
+	}
 
-    @Override
-    public void update(DTNHost thisHost) {
-    }
+	@Override
+	public void update(DTNHost thisHost) {
+	}
 
-    /**
-     * Checks whether a host router is of the same class as target
-     * */
-    public static boolean isTargetRouter(DTNHost host, String targetRouter) {
-        MessageRouter hostRouter = host.getRouter();
-        try {
-            Class<?> targetClass = Class.forName(targetRouter);
-            return targetClass.isAssignableFrom(hostRouter.getClass());
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
+	/**
+	 * Checks whether a host router is of the same class as target
+	 */
+	public static boolean isTargetRouter(DTNHost host, String targetRouter) {
+		MessageRouter hostRouter = host.getRouter();
+		try {
+			Class<?> targetClass = Class.forName(targetRouter);
+			return targetClass.isAssignableFrom(hostRouter.getClass());
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
+	}
 
-    /* SearchingAgentReporting implementations */
+	/* SearchingAgentReporting implementations */
 
-    @Override
-    public double getInitialDiscovery() {
-        return initialDiscovery;
-    }
+	@Override
+	public double getInitialDiscovery() {
+		return initialDiscovery;
+	}
 
-    @Override
-    public Collection<DTNHost> getDiscoveredNodes() {
-        return encounteredSearchables;
-    }
+	@Override
+	public Collection<DTNHost> getDiscoveredNodes() {
+		return discoveredTargets;
+	}
 
-    @Override
-    public String getTargetPrefix() {
-        return targetPrefix;
-    }
+	@Override
+	public String getTargetPrefix() {
+		return targetPrefix;
+	}
 }
